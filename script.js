a0e83a26be79589f37f754ec52faf59a72fcf283
alternadiva <asomeonelse@gmail.com>
2022-02-03 11:51:34 +0000

    select random string from array

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 import {wordsArray} from './data.js';
 
-console.log(wordsArray);
+// DOM
+
+let theWordOutput = document.getElementById("word");
+let inputLetters = document.getElementsByClassName("inputs");
+let submitBtn = document.getElementById("submit");
+let output = document.getElementById("output");
+
+// Random string from the array
+
+let randomWord = "";
+
+function selectWord() {
+    randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    theWordOutput.innerText = randomWord;
+    return randomWord;
+}
+
+window.addEventListener(onload, selectWord);
+
+selectWord();
+
+//console.log(randomWord);
