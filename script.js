905d355422b6db2025c58d88c1828e27108d0d09
alternadiva <asomeonelse@gmail.com>
2022-02-03 14:06:14 +0000

    iterate through inputs and compare with letters

--- script.js
+++ script.js
@@ -3,22 +3,55 @@ import {wordsArray} from './data.js';
 // DOM
 
 let theWordOutput = document.getElementById("word");
-let inputLetters = document.getElementsByClassName("inputs");
+let inputFiels = document.getElementsByClassName("inputs");
 let submitBtn = document.getElementById("submit");
 let output = document.getElementById("output");
 
-// Random string from the array
+// Random string from the array on load event
 
-let randomWord = "";
+let randomWord = [];
 
 function selectWord() {
-    randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-    theWordOutput.innerText = randomWord;
+    let pickedWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    theWordOutput.innerText = pickedWord;
+    randomWord = pickedWord.split("");
+    console.log(randomWord); //Remove later
     return randomWord;
 }
 
-window.addEventListener(onload, selectWord);
+window.addEventListener("load", selectWord);
 
-selectWord();
+// Get input array
 
-//console.log(randomWord);
+let inputArray = [];
+
+function addInputValues() {
+    for (let i = 0; i < inputFiels.length; i++) {
+        let inputValue = inputFiels[i].value;
+        inputArray.push(inputValue.toUpperCase());
+    }
+    return inputArray;
+}
+
+submitBtn.addEventListener("click", addInputValues)
+
+
+// Iterate through inputs and compare with letters
+
+function matchOrNot() {
+    let trueFalse = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        if (randomWord[i] == inputArray[i]) {
+            trueFalse.push("green");
+        }
+        else if (randomWord.includes(inputArray[i])) {
+            trueFalse.push("yellow");
+        }
+        else {
+            trueFalse.push("grey");
+        }
+    }
+    console.log(trueFalse);
+}
+
+submitBtn.addEventListener("click", matchOrNot)
